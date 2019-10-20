f43bb7ec1f113cd17c1f7511a0a7fc4e85cf388d
Yimmie Honrodt <yimmie@honrodt.de>
2019-10-20 14:26:38 +0200

    implemented react-router-dom

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
+
 const Navigation = () => (
   <div>
-    <h1>Navigation</h1>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+        <Link to={ROUTES.HOME}>Home</Link>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    </ul>
   </div>
 );
 export default Navigation;
